bf22733ad840a2f89def979362ec1840be6b4419
Rashidul Hasan <49413046+RashidulHasanRatul@users.noreply.github.com>
2020-12-23 03:50:24 +0600

    Add files via upload
    for in loop

---
+++ forInLoop.js
@@ -0,0 +1,11 @@
+let obj = {
+    name:"Ratul",
+    university:"Daffodil",
+    Id:278,
+    section:"Pc-A",
+}
+
+for(let prop in obj){
+   
+   console.log(`key is ${prop} and value is ${obj[prop]}`);
+}

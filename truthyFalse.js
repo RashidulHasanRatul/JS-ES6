f2683f101f898e08c90f7f772dfee87068427853
Rashidul Hasan <49413046+RashidulHasanRatul@users.noreply.github.com>
2020-12-24 03:22:13 +0600

    Add files via upload
    Truthy Value

---
+++ truthyFalse.js
@@ -0,0 +1,9 @@
+// false,null,NAN,0,undefine,"",  eigula holo falsey value mane false dekhabe
+//  uporer 6 ta sara baki sob truethy 
+let num = "1";
+
+if(num){
+    console.log("this is truthy");
+} else {
+    console.log("this is Falsey");
+}

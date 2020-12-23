431ae7e6fc654b363e8ab004bab8db6f468578ea
Rashidul Hasan <49413046+RashidulHasanRatul@users.noreply.github.com>
2020-12-24 03:22:41 +0600

    Add files via upload
    Ternary

---
+++ TernaryOperator.js
@@ -0,0 +1,9 @@
+let age = 19;
+
+let type = (age>=18) ? "Adult" : "Child";
+
+console.log(type);
+// Jodi outcum true or false hoy taile evabeo likha jay 
+let a = 6;
+let access = a >5 ;
+console.log(access);
